1895c66c53919276e880a4abad680519129e3790
Rub_Rub <D_I123@mail.ru>
2024-10-07 17:01:36 +0300

    added database auto-generation

--- TestConstructor_Task2/Program.cs
+++ TestConstructor_Task2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using System.IO;
 
 namespace TestConstructor_Task2;
 
@@ -9,8 +10,20 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        string _saveFilePath = Environment.CurrentDirectory;
+        string _fileName = "DataBase.json";
+
+        if (!File.Exists(_saveFilePath))
+        {
+            File.Create(_saveFilePath).Dispose();
+        }
+        
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    } 
+        
+        
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
